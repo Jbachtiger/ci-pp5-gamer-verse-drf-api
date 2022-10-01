1d54a58719be5e9f735ff1a0d66730b77ec0cd89
Jbachtiger <jbachtiger@gmail.com>
2022-10-01 19:53:12 +0000

    Add comment detail serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Comment
 
+# DRF-API walkthrough used to get guidance on creating comment model
+# Original code has been modified to suit project purpose
+
 
 class CommentSerializer():
     '''
@@ -20,3 +23,10 @@ class CommentSerializer():
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_on', 'modified_on', 'content']
+
+
+class CommentDetailSerializer(CommentSerializer):
+    '''
+    Serializer for comment model used in Detail View 
+    '''
+    post = serializers.ReadOnlyField(source='post.id')
