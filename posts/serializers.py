c81ebabdde28587e918485d51f00e419c2e7410c
Jbachtiger <jbachtiger@gmail.com>
2022-10-01 15:01:42 +0000

    Add post list view with get method and link urls

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+# Image validation to ensure correct size, height and width of image is uploaded
     def validate(self, value):
         if value.size > 1024 * 1024 * 2:
             raise serializers.ValidationError(
