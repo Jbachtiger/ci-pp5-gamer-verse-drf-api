f615e64b03c3dd21c318e1e7e0fb4b3a4bdcedc2
Jbachtiger <jbachtiger@gmail.com>
2022-10-01 20:23:06 +0000

    Add CommentDetail class to comments view

--- comments/views.py
+++ comments/views.py
@@ -7,14 +7,23 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 
 
 class CommentList(generics.ListCreateAPIView):
-    '''
-    List comments and create a comment if logged in 
-    '''
+    """
+    List comments or create a comment if logged in.
+    """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
 
 
