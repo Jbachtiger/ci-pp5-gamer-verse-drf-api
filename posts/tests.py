cd72417eff20a605a2c9e09babe6ac36564eff38
Jbachtiger <jbachtiger@gmail.com>
2022-10-01 19:11:27 +0000

    Add tests for post detail view

--- posts/tests.py
+++ posts/tests.py
@@ -25,9 +25,43 @@ class PostListViewTests(APITestCase):
         count = Post.objects.count()
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
+  
     def test_user_not_logged_in__cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'Hello World'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
+class PostDetailViewTests(APITestCase):
+    '''
+    Post detail view tests
+    '''
+    def setUp(self):
+        john = User.objects.create_user(username='john', password='drf123')
+        hannah = User.objects.create_user(username='hannah', password='drf123')
+        Post.objects.create(
+            owner=john, title='a title', description='johns content'
+        )
+        Post.objects.create(
+            owner=hannah, title='another title', description='hannahs content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_fetch_a_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='john', password='drf123')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_users_cant_update_a_post_they_dont_own(self):
+        self.client.login(username='hannah', password='drf123')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
