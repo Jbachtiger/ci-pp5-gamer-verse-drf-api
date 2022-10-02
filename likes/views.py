7b5d6cddbb0a724a0757e2fd7a1f9d98e43c8156
Jbachtiger <jbachtiger@gmail.com>
2022-10-02 10:32:11 +0000

    Add LikeList class to likes view

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from pp5_gamer_verse_drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
-# Create your views here.
+# DRF-API walkthrough used to get guidance on creating comment model
+
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
