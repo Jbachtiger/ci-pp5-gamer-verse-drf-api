4fdc186e2d3d576de1e70a400aaf4649eb239bd9
Jbachtiger <jbachtiger@gmail.com>
2022-10-02 13:39:18 +0000

    Add ReviewList class to reviews view

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from pp5_gamer_verse_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+class ReviewList(generics.ListCreateAPIView):
+    '''
+    Create a new review 
+    '''
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+
+    def perform_create(self, serializer):
+        '''
+        Save authenticated reviews 
+        '''
+        serializer.save(owner=self.request.user)
+
+
+
