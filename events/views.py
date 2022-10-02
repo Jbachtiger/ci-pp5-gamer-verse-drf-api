a986fbc5a70d54737fb13ade4609b9fcbc27fcc0
Jbachtiger <jbachtiger@gmail.com>
2022-10-02 16:34:30 +0000

    Add search functionality to events view

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from pp5_gamer_verse_drf_api.permissions import IsOwnerOrReadOnly
 from .models import Event
 from .serializers import EventSerializer
@@ -18,13 +18,22 @@ class EventList(generics.ListCreateAPIView):
         '''
         serializer.save(owner=self.request.user)
 
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+        'city',
+        'address',
+    ]
+
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
-    Retrieve, Update and Destroy events 
+    Retrieve, Update and Destroy events
     '''
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Event.objects.all()
-
-    
